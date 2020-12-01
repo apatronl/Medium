87f9accffddfe451a2567bee1184e5d59ab3221d
Alejandrina Patron <ale7feb@hotmail.com>
2020-12-01 09:39:13 -0500

    Add files via upload

---
+++ Progress View/Progress View/Progress_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_ViewApp.swift
+//  Progress View
+//
+//  Created by Alejandrina Patrón López on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Progress_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
